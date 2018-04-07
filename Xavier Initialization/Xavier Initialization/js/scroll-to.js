93a4f199c386669a79713ccb351178d0518259b2
gkadray <jguo01@risd.edu>
2018-04-07 14:12:36 -0700

    + CSS JS
    + Scroll to
    + css update

---
+++ Xavier Initialization/Xavier Initialization/js/scroll-to.js
@@ -0,0 +1,15 @@
+$(".index").click(function() {
+	
+    var target = $(this).attr('class').split(' ').pop();
+   	target = $("."+target+"-target");
+    toSection($(target));
+    });
+
+
+
+function toSection(section) {
+	
+        var n = section.offset().top;
+        var pos = { 'scrollTop': n }
+        $('html,body').animate(pos, 'slow');
+}
