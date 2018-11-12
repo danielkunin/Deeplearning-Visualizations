621d1bf7c0856e674e2550acd5cc533d2dc0ee34
Daniel Kunin <daniel@DN0a22603a.SUNet>
2018-11-11 17:27:57 -0800

    nn float to int

--- regularization/js/sparsity/nn.js
+++ regularization/js/sparsity/nn.js
@@ -83,7 +83,7 @@ function MNIST(layers) {
 			// get batch
 			tf.tidy(() => {
 				activations['a0'].assign(tf.tensor2d(DATA["images"].slice(index,index + batch)));
-				labels.assign(tf.oneHot(tf.tensor(DATA["labels"].slice(index,index + batch)),10));
+				labels.assign(tf.oneHot(tf.tensor1d(DATA["labels"].slice(index,index + batch), 'int32'), 10).asType('float32'));
 			});
 			// minimize
 			tf.tidy(() => {
